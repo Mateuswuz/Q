36a555d658917923afe2f68e6705151d82f51f70
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-05-23 16:20:37 -0300

    Add files via upload

---
+++ assets/preload/data/introllduction/Logo.lua
@@ -0,0 +1,28 @@
+-- we cant have people locked out of the mod can we?
+-- Fuck Story Mode
+
+function onCreate()
+
+        setProperty('healthBar.alpha', 0)
+        setProperty('canReset', false)
+        
+        makeLuaSprite('logo', 'logo', 0, 0)
+        setObjectCamera("logo", 'hud')
+        screenCenter('logo')
+        setProperty("logo.alpha", 0.00001)
+        addLuaSprite('logo', true)
+end
+
+function onUpdatePost()
+
+        setProperty('health', 1)
+end
+
+function onBeatHit()
+
+        if curBeat == 200 then
+            setProperty("logo.alpha", 1)
+            doTweenX('logoScaleX', 'logo.scale', 0.5, 1, 'backOut')
+            doTweenY('logoScaleY', 'logo.scale', 0.5, 1, 'backOut')
+        end
+end
